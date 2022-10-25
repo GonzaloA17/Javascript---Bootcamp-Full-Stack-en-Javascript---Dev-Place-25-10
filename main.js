0b86c85f9581217d454372c7dced04e6c3b0dead
Gonzalo Ordoñez <114304150+GonzaloA17@users.noreply.github.com>
2022-10-25 14:36:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+/* EJ1*/
+
+/*let meses = new array ('Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre', 'Diciembre')
+
+
+meses.forEach((e) =>{
+    console.log(e);
+
+}) */
+
+
+
+/*Ej2 */
+
+
+
+/*
+
+function parImpar(numero) {
+  if(numero % 2 == 0) {
+    console.log("par") ;
+  }
+  else {
+    console.log("impar");
+  }
+}
+parImpar(13); */
+
+
+/* EJ3 */
+/*
+function info(cadena) {
+ 
+    var resultado = "La cadena \""+cadena+"\" ";
+   
+    // Comprobar mayúsculas y minúsculas
+    if(cadena == cadena.toUpperCase()) {
+      resultado += " está formada sólo por mayúsculas";
+    }
+    else if(cadena == cadena.toLowerCase()) {
+      resultado += " está formada sólo por minúsculas";
+    }
+    else {
+      resultado += " está formada por mayúsculas y minúsculas";
+    }
+   
+    return resultado;
+  }
+
+cadena("HOLA");
+*/
+
+
+/* 4
+var numero = prompt("Introduce un número y se mostrará su factorial");
+var resultado = 1;
+
+for(var i=1; i<=numero; i++) {
+  resultado *= i;
+}
+alert(resultado);
+*/
